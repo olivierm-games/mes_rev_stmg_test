d112af3a5ba4c71a59571e35d72c53b943afe670
olivierm-games <oliviermu06@gmail.com>
2022-01-22 23:47:38 +0100

    many changes

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,16 @@ function onMenu(container) {
     // prompt("0");
   }
 }
+
+// function onMenu() {
+//   container.classList.toggle("anim-menu");
+//   if(container.classList.contains("anim-menu")) {
+//     document.getElementById("side-nav").style.right="0";
+//     // console.log("100%");
+//     // promt("100%");
+//   } else {
+//     document.getElementById("side-nav").style.right="100%";
+//     // console.log("0");
+//     // prompt("0");
+//   }
+// }
