51fba9992328c8a811e2ae7de15c69932ebe5d57
olivierm-games <oliviermu06@gmail.com>
2022-01-23 00:33:39 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -16,13 +16,13 @@ function onMenu() {
   isSideNaveOpen=!isSideNaveOpen;
   if(isSideNaveOpen) {
     document.getElementById("side-nav").style.width="250px";
-    if(document.matchMedia('(max-width: 921px)').matches) {
+    if(window.matchMedia('(max-width: 921px)').matches) {
       // phones, portrait tablets. Set background color of main and header to dark. E5DDFF
       document.getElementById("main-page").style.backgroundImage='linearGradient(90deg, #E5DDFF66, #E5DDFF99, #E5DDFFDD)';
     }
   } else {
     document.getElementById("side-nav").style.width="0";
-    if(document.matchMedia('(max-width: 921px)').matches) {
+    if(window.matchMedia('(max-width: 921px)').matches) {
       document.getElementById("main-page").style.backgroundImage='#E5DDFF';
     }
   }
