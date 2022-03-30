bdfa795a270d18fa5489552afd35438b45629d10
olivierm-games <56362374+olivierm-games@users.noreply.github.com>
2022-03-30 13:37:20 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,12 @@
-// function onMenu(container) {
-//   container.classList.toggle("anim-menu");
-//   if(container.classList.contains("anim-menu")) {
-//     document.getElementById("side-nav").style.right="0";
-//     // console.log("100%");
-//     // promt("100%");
-//   } else {
-//     document.getElementById("side-nav").style.right="100%";
-//     // console.log("0");
-//     // prompt("0");
-//   }
-// }
-let isSideNaveOpen=false;
+function onDesktopMenu() {
+   document.getElementsByTagName("aside")[0].classList.toggle("open");
+   document.getElementById("menu-svg").classList.toggle("anim");
+   document.getElementsByTagName("main")[0].classList.toggle("open");
+}
 
-function onMenu() {
-  isSideNaveOpen=!isSideNaveOpen;
-  console.log("isSideNaveOpen: "+isSideNaveOpen);
-  if(isSideNaveOpen) {
-    document.getElementById("side-nav").style.width="250px";
-    if(window.matchMedia('(max-width: 921px)').matches) {
-      // phones, portrait tablets. Set background color of main and header to dark. E5DDFF
-      document.getElementById("main-page").style.backgroundImage='linearGradient(90deg, #E5DDFF66, #E5DDFF99, #E5DDFFDD)';
-    }
-  } else {
-    document.getElementById("side-nav").style.width="0";
-    if(window.matchMedia('(max-width: 921px)').matches) {
-      document.getElementById("main-page").style.backgroundImage='#E5DDFF';
-    }
-  }
+function toggleJustOpened() { document.getElementsByTagName("aside")[0].classList.toggle("just-opened"); }
+
+function onPhoneMenu(toggleJustOpened) {
+   onDesktopMenu();
+   if(toggleJustOpened) document.getElementsByTagName("aside")[0].classList.toggle("just-opened");
 }
