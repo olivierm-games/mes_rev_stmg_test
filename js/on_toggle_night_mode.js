91623f51aa916c976a4ef4e68d4cd5a250d7de62
olivierm-games <56362374+olivierm-games@users.noreply.github.com>
2022-03-30 13:37:53 +0200

    Update on_toggle_night_mode.js

--- js/on_toggle_night_mode.js
+++ js/on_toggle_night_mode.js
@@ -1,15 +1,4 @@
 function onToggleNightMode() {
-    var elements=document.getElementsByClassName("night-mode-element");
-    i=elements.length;
-    // console.log("toggle, "+i+" elements");
-    for(j=0;j<i;j++) {
-      elements[j].classList.toggle("night-mode");
-    }
-    if(elements[i-1].classList.contains("night-mode")) {
-      // console.log("toggle to night");
-      document.getElementById("night-mode").innerHTML="light_mode";
-    } else {
-      // console.log("toggle to light");
-      document.getElementById("night-mode").innerHTML="dark_mode";
-    }
+   if(!document.getElementsByClassName("night-mode-element")[0].classList.contains("night-mode")) toggleNightMode("&#xe518;", "clair");
+   else toggleNightMode("&#xe51c;", "sombre");
 }
