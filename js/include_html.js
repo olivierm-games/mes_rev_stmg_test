013bbcce99a3a10032a34a7478d8ac2436de0497
olivierm-games <oliviermu06@gmail.com>
2022-03-30 22:44:38 +0200

    promise added

--- js/include_html.js
+++ js/include_html.js
@@ -1,6 +1,6 @@
-// includeHTML();
+includeHTML();
 
-function includeHTML(path) {
+function includeHTML() {
    var elements=document.getElementsByTagName("*");
    var size=elements.length;
    for (i = 0; i < size; i++) {
@@ -10,19 +10,7 @@ function includeHTML(path) {
          var xhttp=new XMLHttpRequest();
          xhttp.onreadystatechange=function () {
             if(this.readyState==4) {
-               if(this.status==200) {
-                  elements[j].innerHTML=this.responseText;
-
-
-                  // TODO: Set path to links
-                  var links=document.getElementsByTagName("a");
-                  var linksCount=links.length;
-                  for(var linkI=0; linkI<linksCount; linkI++) {
-                     console.log("href: "+links[linkI].href);
-                  }
-
-
-               }
+               if(this.status==200) { elements[j].innerHTML=this.responseText; }
                else if(this.status==404) { elements[j].innerHTML="Page non trouvée."; }
                elements[j].removeAttribute("w3-include-html");
                includeHTML();
---
+++ js/wait_for_elm.js
@@ -0,0 +1,59 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+
+
+           mutations.addedNodes.find(node => {
+             if(node.matchesSelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+             }
+          });
+
+
+            // if (document.querySelector(selector)) {
+            //     resolve(document.querySelector(selector));
+            //     observer.disconnect();
+            // }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+// function waitForElm(selector) {
+//    let observer = new MutationObserver(mutations => {
+//
+//      for(let mutation of mutations) {
+//        // examiner les nouveaux nœuds, y a-t-il quelque chose à mettre en évidence ?
+//
+//        for(let node of mutation.addedNodes) {
+//          // nous ne suivons que les éléments, nous sautons les autres nœuds (par exemple les nœuds de texte)
+//          if (!(node instanceof HTMLElement)) continue;
+//
+//          // vérifier que l'élément inséré est un extrait de code
+//          if (node.matches(selector)) {
+//             disconnect();
+//            // Prism.highlightElement(node);
+//          }
+//
+//          // ou peut-être qu'il y a un extrait de code quelque part dans son sous-arbre ?
+//          // for(let elem of node.querySelectorAll('pre[class*="language-"]')) {
+//          //   Prism.highlightElement(elem);
+//          // }
+//        }
+//      }
+//
+//    });
+//
+//    // let demoElem = document.getElementById('highlight-demo');
+//
+//    observer.observe(document.body, {childList: true, subtree: true});
+// }
