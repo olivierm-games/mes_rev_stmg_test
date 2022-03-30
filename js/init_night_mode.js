63968d13b851aac872b3e559bbe710b9ac780787
olivierm-games <56362374+olivierm-games@users.noreply.github.com>
2022-03-30 13:36:31 +0200

    Update init_night_mode.js

--- js/init_night_mode.js
+++ js/init_night_mode.js
@@ -1,23 +1,24 @@
 if(window.matchMedia) {
-
-
-  window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
-    toggleNightModeClasses();
-    if(document.getElementById("header").classList.contains("night-mode")) document.getElementById("night-mode").innerHTML="light_mode";
-    else document.getElementById("night-mode").innerHTML="dark_mode";
-  });
-
-
-  if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    toggleNightModeClasses();
-    document.getElementById("night-mode").innerHTML="light_mode";
-  }
+   window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
+      if(e.matches) toggleNightMode("&#xe518;", "clair");
+      else toggleNightMode("&#xe51c;", "sombre");
+   });
+   if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleNightMode("&#xe518;", "clair");
 }
 
 function toggleNightModeClasses() {
-  const elements=document.getElementsByClassName("night-mode-element");
-    i=elements.length;
-    for(j=0;j<i;j++) {
-      elements[j].classList.toggle("night-mode");
-    }
+   const elements=document.getElementsByClassName("night-mode-element");
+   i=elements.length;
+   for(j=0;j<i;j++) elements[j].classList.toggle("night-mode");
+}
+
+function toggleNightModeBtn(innerHTML, title) {
+   const nightModeBtn=document.getElementById("night-mode");
+   nightModeBtn.innerHTML=innerHTML;
+   nightModeBtn.title="Passer en mode "+title;
+}
+
+function toggleNightMode(innerHTML, title) {
+   toggleNightModeClasses();
+   toggleNightModeBtn(innerHTML, title);
 }
