a750fa0319c3111416728c5c0c2d30060bb7fabe
olivierm-games <oliviermu06@gmail.com>
2022-03-30 20:30:01 +0200

    include update

--- js/include_html.js
+++ js/include_html.js
@@ -1,6 +1,6 @@
-includeHTML();
+// includeHTML();
 
-function includeHTML() {
+function includeHTML(path) {
    var elements=document.getElementsByTagName("*");
    var size=elements.length;
    for (i = 0; i < size; i++) {
@@ -10,7 +10,19 @@ function includeHTML() {
          var xhttp=new XMLHttpRequest();
          xhttp.onreadystatechange=function () {
             if(this.readyState==4) {
-               if(this.status==200) { elements[j].innerHTML=this.responseText; }
+               if(this.status==200) {
+                  elements[j].innerHTML=this.responseText;
+
+
+                  // TODO: Set path to links
+                  var links=document.getElementsByTagName("a");
+                  var linksCount=links.length;
+                  for(var linkI=0; linkI<linksCount; linkI++) {
+                     console.log("href: "+links[linkI].href);
+                  }
+
+
+               }
                else if(this.status==404) { elements[j].innerHTML="Page non trouvée."; }
                elements[j].removeAttribute("w3-include-html");
                includeHTML();
