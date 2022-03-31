4ca4022632e805f06c12b549c0d18455d4f5d6ce
olivierm-games <oliviermu06@gmail.com>
2022-03-31 23:20:48 +0200

    reworked night-mode impl

--- js/night_mode.js
+++ js/night_mode.js
@@ -1,14 +1,15 @@
-// var isNightMode;
+var storedNightMode;
 
 
 window.onload=function() {
-   var item=localStorage.getItem("night-mode");
-   console.log(item);
-   if(item!==null) {
-      console.log("A night mode preference has been set by the user");
-      if(localStorage.getItem("night-mode")==="true") toggleNightMode("&#xe518;", "clair");
-      else toggleNightMode("&#xe51c;", "sombre");
-   }
+   // var item=localStorage.getItem("night-mode");
+   storedNightMode=localStorage.getItem("night-mode");
+   console.log(storedNightMode);
+   // if(item!==null) {
+   //    console.log("A night mode preference has been set by the user");
+   //    if(localStorage.getItem("night-mode")==="true") toggleNightMode("&#xe518;", "clair");
+   //    else toggleNightMode("&#xe51c;", "sombre");
+   // }
    // initNightMode();
 }
 
@@ -19,17 +20,31 @@ window.onload=function() {
 function initNightMode() {
 
 
-   var item=localStorage.getItem("night-mode");
-   console.log(item);
-   if(item===null && window.matchMedia) {
+   if(storedNightMode!==null) {
+      if(storedNightMode==="true") toggleNightMode(true, false);
+      else toggleNightMode(false, false);
+   } else if(window.matchMedia) {
       window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
-         var isNightMode=e.matches;
-         localStorage.setItem("night-mode", isNightMode);
-         if(isNightMode) toggleNightMode("&#xe518;", "clair");
-         else toggleNightMode("&#xe51c;", "sombre");
+         // var isNightMode=e.matches;
+         // localStorage.setItem("night-mode", isNightMode);
+         if(e.matches) toggleNightMode(true, true);
+         else toggleNightMode(false, true);
       });
-      if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleNightMode("&#xe518;", "clair");
+      if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleNightMode(true, false);
    }
+
+
+   // var item=localStorage.getItem("night-mode");
+   // console.log(item);
+   // if(item===null && window.matchMedia) {
+   //    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
+   //       var isNightMode=e.matches;
+   //       localStorage.setItem("night-mode", isNightMode);
+   //       if(isNightMode) toggleNightMode("&#xe518;", "clair");
+   //       else toggleNightMode("&#xe51c;", "sombre");
+   //    });
+   //    if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleNightMode("&#xe518;", "clair");
+   // }
 
 
 
@@ -54,7 +69,14 @@ function toggleNightModeBtn(innerHTML, title) {
    nightModeBtn.title="Passer en mode "+title;
 }
 
-function toggleNightMode(innerHTML, title) {
+// function toggleNightMode(innerHTML, title) {
+//    toggleNightModeClasses();
+//    toggleNightModeBtn(innerHTML, title);
+// }
+
+function toggleNightMode(night, persist) {
    toggleNightModeClasses();
-   toggleNightModeBtn(innerHTML, title);
+   if(night) toggleNightModeBtn("&#xe518;", "clair");
+   else toggleNightModeBtn("&#xe51c;", "sombre");
+   if(persist) localStorage.setItem("night-mode", storedNightMode);
 }
--- js/on_toggle_night_mode.js
+++ js/on_toggle_night_mode.js
@@ -1,4 +1,6 @@
 function onToggleNightMode() {
-   if(!document.getElementsByClassName("night-mode-element")[0].classList.contains("night-mode")) toggleNightMode("&#xe518;", "clair");
-   else toggleNightMode("&#xe51c;", "sombre");
+   if(!document.getElementsByClassName("night-mode-element")[0].classList.contains("night-mode")) toggleNightMode(true, true);
+   // if(!document.getElementsByClassName("night-mode-element")[0].classList.contains("night-mode")) toggleNightMode("&#xe518;", "clair");
+   else toggleNightMode(false, true);
+   // else toggleNightMode("&#xe51c;", "sombre");
 }
