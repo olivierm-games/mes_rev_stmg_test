f25c22f7dec6e0ea3c80d0297fc21b0121b4f070
olivierm-games <oliviermu06@gmail.com>
2022-04-07 18:59:32 +0200

    removed html,  body, main height

--- js/night_mode.js
+++ js/night_mode.js
@@ -6,28 +6,14 @@ let storedNightMode=null;
 /** by default 1 for day mode; 0 is night mode. Updated after toggling UI and eventually after storage*/
 let nightModeIndex=1;
 
-window.onload=function() {
-   storedNightMode=localStorage.getItem("night-mode");
-   // console.log("onload, stored: "+storedNightMode);
-}
+window.onload=function() { storedNightMode=localStorage.getItem("night-mode"); }
 
 function initNightMode() {
-   // console.log("initNightMode, stored: "+storedNightMode);
-   if(storedNightMode===null) {
-      storedNightMode=localStorage.getItem("night-mode");
-      console.log("not saved? "+storedNightMode);
-   }
-   // storedNightMode=localStorage.getItem("night-mode");
+   if(storedNightMode===null) storedNightMode=localStorage.getItem("night-mode");
    const matchMedia=window.matchMedia;
    if(storedNightMode!==null) {
-      if(storedNightMode==="night") {
-         console.log("saved night, toggling. Shouldn't if refreshing?");
-         toggleNightMode(); // since by default night mode is off (day on)
-      } else console.log("saved light");
-   } else if(matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      // console.log("match night media");
-      toggleNightMode();
-   }
+      if(storedNightMode==="night") toggleNightMode(); // since by default night mode is off (day on)
+   } else if(matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) toggleNightMode();
    if(matchMedia) window.matchMedia("(prefers-color-scheme: dark)").addListener(e => toggleNightMode());
 }
 
@@ -37,20 +23,18 @@ function onToggleNightMode() {
 }
 
 function toggleNightMode() {
-   // console.log("toggleNightMode");
    toggleNightModeBtn();
    toggleNightModeClasses();
    updateIndex();
 }
 
 function saveInLocalStorage() {
-   // console.log("saveInLocalStorage, storing: "+STORAGE_KEYS[nightModeIndex]);
    localStorage.setItem("night-mode", STORAGE_KEYS[nightModeIndex]);
 }
 
 function toggleNightModeClasses() {
    const elements=document.getElementsByClassName("night-mode-element");
-   var elementsCount=elements.length;
+   const elementsCount=elements.length;
    for(j=0;j<elementsCount;j++) elements[j].classList.toggle("night-mode");
 }
 
