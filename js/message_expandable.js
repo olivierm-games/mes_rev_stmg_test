df9dee0c2a310b13c222ac6a932465a3b61c526f
olivierm-games <oliviermu06@gmail.com>
2022-04-18 03:16:47 +0200

    summary droit 1 update

--- js/message_expandable.js
+++ js/message_expandable.js
@@ -1,6 +1,5 @@
 const messageTip=document.querySelector(".message-tip");
 const messageSlider=document.querySelector(".message-slider");
-// const main=document.querySelector('main');
 
 messageTip.addEventListener("click", function() {
    var tipWidthDeltaSliderWidth=messageTip.clientWidth-messageSlider.clientWidth;
@@ -10,11 +9,8 @@ messageTip.addEventListener("click", function() {
    var messageMaxW=windowInnerW-messageMarginD-tipWidthDeltaSliderWidth;
    this.classList.toggle("closed");
    messageSlider.style.maxWidth=messageSlider.style.maxWidth!=="0px"?"0px":messageMaxW+"px";
-   // messageSlider.style.maxHeight=messageSlider.scrollHeight+"px";
 });
 
-// const sliderMaxHeight=messageSlider.scrollHeight;
-console.log("messageSlider.scrollHeight: "+messageSlider.scrollHeight);
 messageSlider.style.height=messageSlider.scrollHeight+"px";
 messageTip.classList.toggle("closed");
 messageSlider.style.maxWidth="0px";
