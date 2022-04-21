ee325b55962c31c76daf556426193f30cb792793
olivierm-games <oliviermu06@gmail.com>
2022-04-22 01:50:22 +0200

    c

--- js/fab_back_to_top.js
+++ js/fab_back_to_top.js
@@ -8,6 +8,20 @@ function setFabBackToTop() {
 
 window.onscroll=function() {
    // if(allContainer.scrollTop>100) {
-      fab.style.opacity=(document.documentElement.scrollTop-100)/100;
+   var scrollTop=allContainer.scrollTop;
+   // var scrollTop=document.documentElement.scrollTop;
+   console.log("scrollTop: "+scrollTop);
+   fab.style.opacity=(scrollTop-100)/100;
    // }
+   // clientHeight, scrollTop
+   var docHeight=allContainer.offsetHeight;
+   console.log("clientHeight: "+docHeight);
+   var fabBottom=Math.max(48, scrollTop-docHeight-148);
+   fab.style.bottom=fabBottom+"px";
+   // if(scrollTop>=(clientHeight-150)) {
+   //    fab.style.bottom="px";
+   // } else fab.style.bottom="48px";
 }
+// scrollTop-(clientHeight-150)>0 =>
+// 180-(380-150)=180-230=-50<0 => scroll position is higher than the limit
+// 280-(380-148)=280-232=48>0 => scroll position is lower than the limit, fab should be at scrollTop-clientHeight-148
