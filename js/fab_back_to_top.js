399f514bff0d8b59e148626573a7b9508c068a92
olivierm-games <oliviermu06@gmail.com>
2022-04-22 16:42:50 +0200

    bug

--- js/fab_back_to_top.js
+++ js/fab_back_to_top.js
@@ -1,10 +1,10 @@
 let allContainer=null;
-// let fab=null;
+let fab=null;
 let docElmScrollTop=0, allContainerHeight=0, windowHeight=0;
 
-function setFabBackToTop(fab) {
+function setFabBackToTop() {
    allContainer=document.getElementById('all-container');
-   // fab=document.getElementById('fab-back-to-top');
+   fab=document.getElementById('fab-back-to-top');
    allContainerHeight=allContainer.offsetHeight;
    windowHeight=window.innerHeight;
    window.addEventListener(
