9527983647b713766b88446b8f72f9617a9dcb2f
olivierm-games <oliviermu06@gmail.com>
2022-04-23 00:01:45 +0200

    cours

--- js/message_expandable.js
+++ js/message_expandable.js
@@ -1,16 +1,16 @@
 const messageTip=document.querySelector(".message-tip");
 const messageSlider=document.querySelector(".message-slider");
-
+//
 messageTip.addEventListener("click", function() {
-   var tipWidthDeltaSliderWidth=messageTip.clientWidth-messageSlider.clientWidth;
-   var messageMarginS = window.getComputedStyle(messageTip, null).getPropertyValue('margin-left');
-   var messageMarginD=Number(messageMarginS.slice(0, -2))*2;
-   var windowInnerW=window.innerWidth;
-   var messageMaxW=windowInnerW-messageMarginD-tipWidthDeltaSliderWidth;
+   // var tipWidthDeltaSliderWidth=messageTip.clientWidth-messageSlider.clientWidth;
+   // var messageMarginS = window.getComputedStyle(messageTip, null).getPropertyValue('margin-left');
+   // var messageMarginD=Number(messageMarginS.slice(0, -2))*2;
+   // var windowInnerW=window.innerWidth;
+   // var messageMaxW=windowInnerW-messageMarginD-tipWidthDeltaSliderWidth;
    this.classList.toggle("closed");
-   messageSlider.style.maxWidth=messageSlider.style.maxWidth!=="0px"?"0px":messageMaxW+"px";
+   // messageSlider.style.maxWidth=messageSlider.style.maxWidth!=="0px"?"0px":messageMaxW+"px";
 });
-
-messageSlider.style.height=messageSlider.scrollHeight+"px";
-messageTip.classList.toggle("closed");
-messageSlider.style.maxWidth="0px";
+//
+// messageSlider.style.height=messageSlider.scrollHeight+"px";
+// messageTip.classList.toggle("closed");
+// messageSlider.style.maxWidth="0px";
