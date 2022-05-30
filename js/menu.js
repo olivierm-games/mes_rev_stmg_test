bf1c97ca605cf02c0942c1cd51c2638d0315a711
olivierm-games <oliviermu06@gmail.com>
2022-05-30 10:24:23 +0200

    changed eco mana directory names c1 eco 1ere

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,4 @@
-// function onDesktopMenu() {
-//    document.getElementsByTagName("aside")[0].classList.toggle("open");
-//    document.getElementById("menu-svg").classList.toggle("anim");
-//    document.getElementsByTagName("main")[0].classList.toggle("open");
-// }
-//
-// function toggleJustOpened() { document.getElementsByTagName("aside")[0].classList.toggle("just-opened"); }
-//
-// function onPhoneMenu(toggleJustOpened) {
-//    onDesktopMenu();
-//    if(toggleJustOpened) document.getElementsByTagName("aside")[0].classList.toggle("just-opened");
-// }
-
 function onMenu() {
-   // console.log("onMenu");
-   // document.getElementsByTagName("aside")[0].classList.toggle("open");
-   // document.getElementById("menu-svg").classList.toggle("anim");
-   // document.getElementsByTagName("main")[0].classList.toggle("open");
    requestMenuTick();
 }
 
--- js/popup_info.js
+++ js/popup_info.js
@@ -1,11 +1,23 @@
-const popupTargets=document.getElementsByClassName('popup-target');
+const popupTargets=document.getElementsByClassName('popup-target-text');
+// const popupTargets=document.getElementsByClassName('popup-target');
+
+const popupContainers=document.getElementsByClassName('popup-container');
+
 const targetsCount=popupTargets.length;
 
 for(var i=0; i<targetsCount; i++) {
    popupTargets[i].addEventListener('mouseenter', function() {toggleShow(this)});
+   popupContainers[i].addEventListener('mouseenter', function() {this.classList.add('show');});
+
    popupTargets[i].addEventListener('mouseleave', function() {toggleShow(this)});
+   popupContainers[i].addEventListener('mouseleave', function() {this.classList.remove('show');});
+
    popupTargets[i].addEventListener('touchstart', function() {toggleShow(this), {passive: true}});
+   // popupContainers[i].addEventListener('touchstart', function() {this.classList.add('show');, {passive: true}});
+
    popupTargets[i].addEventListener('touchend', function() {toggleShow(this), {passive: true}});
+   // popupContainers[i].addEventListener('touchend', function() {toggleShow(this), {passive: true}});
 }
 
-function toggleShow(target) { target.lastElementChild.classList.toggle('show'); }
+function toggleShow(target) { target.nextElementSibling.classList.toggle('show'); }
+// function toggleShow(target) { target.lastElementChild.classList.toggle('show'); }
