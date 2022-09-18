8a35d4737d708b47b2376fcbe40662f575536cd2
olivierm-games <oliviermu06@gmail.com>
2022-09-18 23:02:52 +0200

    fix

--- js/message_expandable.js
+++ js/message_expandable.js
@@ -1,6 +1,6 @@
-// const messageTip=document.querySelector(".message-tip");
+const messageTip=document.querySelector(".message-tip");
 // const messageSlider=document.querySelector(".message-slider");
-//
-// messageTip.addEventListener("click", function() {
-//    this.classList.toggle("closed");
-// });
+
+messageTip.addEventListener("click", function() {
+   this.classList.toggle("closed");
+});
