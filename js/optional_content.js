4da65b0a194e922d05a496251c37fa7b74e5e687
olivierm-games <oliviermu06@gmail.com>
2022-09-18 21:00:19 +0200

    removed obsolete files

--- js/optional_content.js
+++ js/optional_content.js
@@ -1,15 +1,12 @@
-const optionals=document.getElementsByClassName('optional');
-const optionalsCount=optionals.length;
-for(var i=0;i<optionalsCount;i++) {
-   // console.log("optionals["+i+"]: "+optionals[i]);
-   const iFinal=i;
-   const header=optionals[i].firstElementChild;
-   const body=optionals[i].lastElementChild;
-   // console.log("header: "+header);
-   // console.log("body: "+body);
-   header.addEventListener('click', function() {
-      this.classList.toggle('closed');
-      if(body.style.maxHeight) body.style.maxHeight=null;
-      else body.style.maxHeight=body.scrollHeight+"px";
-   });
-}
+// const optionals=document.getElementsByClassName('optional');
+// const optionalsCount=optionals.length;
+// for(var i=0;i<optionalsCount;i++) {
+//    const iFinal=i;
+//    const header=optionals[i].firstElementChild;
+//    const body=optionals[i].lastElementChild;
+//    header.addEventListener('click', function() {
+//       this.classList.toggle('closed');
+//       if(body.style.maxHeight) body.style.maxHeight=null;
+//       else body.style.maxHeight=body.scrollHeight+"px";
+//    });
+// }
--- js/popup_info.js
+++ js/popup_info.js
@@ -1,23 +1,19 @@
-const popupTargets=document.getElementsByClassName('popup-target-text');
-// const popupTargets=document.getElementsByClassName('popup-target');
-
-const popupContainers=document.getElementsByClassName('popup-container');
-
-const targetsCount=popupTargets.length;
-
-for(var i=0; i<targetsCount; i++) {
-   popupTargets[i].addEventListener('mouseenter', function() {toggleShow(this)});
-   popupContainers[i].addEventListener('mouseenter', function() {this.classList.add('show');});
-
-   popupTargets[i].addEventListener('mouseleave', function() {toggleShow(this)});
-   popupContainers[i].addEventListener('mouseleave', function() {this.classList.remove('show');});
-
-   popupTargets[i].addEventListener('touchstart', function() {toggleShow(this), {passive: true}});
-   // popupContainers[i].addEventListener('touchstart', function() {this.classList.add('show');, {passive: true}});
-
-   popupTargets[i].addEventListener('touchend', function() {toggleShow(this), {passive: true}});
-   // popupContainers[i].addEventListener('touchend', function() {toggleShow(this), {passive: true}});
-}
-
-function toggleShow(target) { target.nextElementSibling.nextElementSibling.classList.toggle('show'); }
-// function toggleShow(target) { target.lastElementChild.classList.toggle('show'); }
+// const popupTargets=document.getElementsByClassName('popup-target-text');
+//
+// const popupContainers=document.getElementsByClassName('popup-container');
+//
+// const targetsCount=popupTargets.length;
+//
+// for(var i=0; i<targetsCount; i++) {
+//    popupTargets[i].addEventListener('mouseenter', function() {toggleShow(this)});
+//    popupContainers[i].addEventListener('mouseenter', function() {this.classList.add('show');});
+//
+//    popupTargets[i].addEventListener('mouseleave', function() {toggleShow(this)});
+//    popupContainers[i].addEventListener('mouseleave', function() {this.classList.remove('show');});
+//
+//    popupTargets[i].addEventListener('touchstart', function() {toggleShow(this), {passive: true}});
+//
+//    popupTargets[i].addEventListener('touchend', function() {toggleShow(this), {passive: true}});
+// }
+//
+// function toggleShow(target) { target.nextElementSibling.nextElementSibling.classList.toggle('show'); }
