da97fc187397799b062f71638be7d6f97882a56f
olivierm-games <oliviermu06@gmail.com>
2022-09-28 21:53:20 +0200

    update

--- js/fab_back_to_top.js
+++ js/fab_back_to_top.js
@@ -1,22 +1,29 @@
 let allContainer=null;
 let fab=null;
-let docElmScrollTop=0, allContainerHeight=0, windowHeight=0;
+// let docElmScrollTop=0,
+// let allContainerHeight=0, windowHeight=0;
+
+let maxFabY=0;
 
 function setFabBackToTop() {
    allContainer=document.getElementById('all-container');
    fab=document.getElementById('fab-back-to-top');
-   allContainerHeight=allContainer.offsetHeight;
-   windowHeight=window.innerHeight;
+   // allContainerHeight=allContainer.offsetHeight;
+   // windowHeight=window.innerHeight;
+
+   maxFabY=allContainer.offsetHeight-window.innerHeight-148;
+
    window.addEventListener(
       'scroll',
-      () => styleFab(),
+      () => styleFab(document.documentElement.scrollTop),
       { passive: true }
    );
    styleFab();
 }
 
-function styleFab() {
-   var docElmScrollTop=document.documentElement.scrollTop;
-   fab.style.opacity=Math.min(.9, (docElmScrollTop-100)/100);
-   fab.style.bottom=Math.max(48, docElmScrollTop-(allContainerHeight-windowHeight-148))+"px";
+function styleFab(scrollTop) {
+   // var docElmScrollTop=document.documentElement.scrollTop;
+   fab.style.opacity=Math.min(.7, (scrollTop-100)/100);
+   fab.style.bottom=Math.max(48, scrollTop-maxFabY)+"px";
+   // fab.style.bottom=Math.max(48, scrollTop-(allContainerHeight-windowHeight-148))+"px";
 }
