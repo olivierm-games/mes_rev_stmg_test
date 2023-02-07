96fd52650ac0f382675dbb2a2b8571f63a610288
olivierm-games <oliviermu06@gmail.com>
2023-02-08 00:21:50 +0100

    fallback fonts

--- js/web-font-loader.js
+++ js/web-font-loader.js
@@ -1,6 +1,6 @@
 WebFont.load({
   google: {
-     families: ['Roboto', 'Balsamiq Sans', 'Comfortaa']
+     families: ['Roboto', 'Balsamiq Sans', 'Comfortaa', , 'Yellowtail', 'Oregano']
     // families: ['Roboto', 'Oregano', 'Road Rage', 'Poiret One', 'Yellowtail', 'Lilita One', 'Balsamiq Sans', 'Dancing Script', 'Kaushan Script', 'Itim', 'Comfortaa']
   }
 });
