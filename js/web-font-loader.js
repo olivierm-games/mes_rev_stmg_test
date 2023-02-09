cf12db7f5db34df8483a471cea256287b2fea530
olivierm-games <oliviermu06@gmail.com>
2023-02-09 04:10:52 +0100

    d

--- js/web-font-loader.js
+++ js/web-font-loader.js
@@ -1,6 +1,5 @@
 WebFont.load({
   google: {
      families: ['Roboto', 'Balsamiq Sans', 'Comfortaa', 'Oregano', 'Road Rage']
-    // families: ['Roboto', 'Oregano', 'Road Rage', 'Poiret One', 'Yellowtail', 'Lilita One', 'Balsamiq Sans', 'Dancing Script', 'Kaushan Script', 'Itim', 'Comfortaa']
   }
 });
